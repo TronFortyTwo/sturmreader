03ad14c303d6a3243b5c1819aaafbf8ee32343ff
Emanuele Sorce <emanuele.sorce@hotmail.com>
2019-10-29 17:03:13 +0000

    Basic stripped down version of Calibre will be included in the click package

---
+++ installcalibre.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p "$INSTALL_DIR"/calibre/PYTHON
+mkdir -p "$INSTALL_DIR"/calibre/RESOURCES
+mkdir -p "$INSTALL_DIR"/calibre/EXECUTABLES
+
+cp -r -p /usr/lib/calibre/* "$INSTALL_DIR"/calibre/PYTHON
+cp -r -p /usr/share/calibre/* "$INSTALL_DIR"/calibre/RESOURCES
+cp -r -p "$ROOT"/calibre/* "$INSTALL_DIR"/calibre/EXECUTABLES
+cp -r -p /usr/bin/python2.7 "$INSTALL_DIR"/calibre/EXECUTABLES
+
+find "$INSTALL_DIR"/calibre/ -name '*.png' -delete
