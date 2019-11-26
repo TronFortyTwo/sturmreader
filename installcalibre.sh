4744d5c0cd6656e9408dd7b81cbd8bf80966eab9
Emanuele Sorce <emanuele.sorce@hotmail.com>
2019-11-26 13:12:30 +0000

    Fixes other python import error

--- installcalibre.sh
+++ installcalibre.sh
@@ -7,10 +7,7 @@ mkdir -p "$INSTALL_DIR"/calibre/PYTHON/lib/python2.7/lib-dynload/
 cp -r -p /usr/lib/calibre/* "$INSTALL_DIR"/calibre/PYTHON/lib/calibre
 # Do resources really are needed? yes, at least the .py files
 cp -r -p /usr/share/calibre/* "$INSTALL_DIR"/calibre/RESOURCES
-cp -r -p /usr/lib/python2.7/*.py "$INSTALL_DIR"/calibre/PYTHON/lib/python2.7/
-cp -r -p /usr/lib/python2.7/plat-arm-linux-gnueabihf/*.py "$INSTALL_DIR"/calibre/PYTHON/lib/python2.7/
-cp -r -p /usr/lib/python2.7/lib-dynload/future_builtins* "$INSTALL_DIR"/calibre/PYTHON/lib/python2.7/
-cp -r -p /usr/lib/python2.7/dist-packages/ "$INSTALL_DIR"/calibre/PYTHON/lib/python2.7/
+cp -r -p /usr/lib/python2.7/ "$INSTALL_DIR"/calibre/PYTHON/lib/
 
 # we copy only the calibre commands we use
 cp -r -p calibre/calibredb "$INSTALL_DIR"/calibre/
@@ -19,3 +16,5 @@ cp -r -p calibre/calibre-customize "$INSTALL_DIR"/calibre/
 # we delete resources we don't use
 find "$INSTALL_DIR"/calibre/RESOURCES -type f \( -name "*.ico" -o -name "*.zip" -o -name "*.png" -o -name ".svg" -o -name "*.jpg" -o -name "*.gif" -o -name "*.css" -o -name "*.html" -o -name "*.htm" -o -name "*.js" \) -delete
 find "$INSTALL_DIR"/calibre/RESOURCES -empty -type d -delete
+
+find "$INSTALL_DIR"/calibre/PYTHON -type f -name "*.pyc" -delete
