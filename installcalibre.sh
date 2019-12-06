06ff555ae72c629eb1dd55b2726fdac48c2ee193
Emanuele Sorce <emanuele.sorce@hotmail.com>
2019-12-06 12:39:41 +0000

    Fixed OSERROR13 on calibre cli launch

--- installcalibre.sh
+++ installcalibre.sh
@@ -12,6 +12,13 @@ cp -r -p /usr/lib/python2.7/ "$INSTALL_DIR"/calibre/PYTHON/lib/
 # we copy only the calibre commands we use
 cp -r -p calibre/calibredb "$INSTALL_DIR"/calibre/
 cp -r -p calibre/calibre-customize "$INSTALL_DIR"/calibre/
+cp -r -p calibre/ebook-convert "$INSTALL_DIR"/calibre/
+
+# fix OSERR 13 error
+sed -i "s/libpthread_path = ctypes.util.find_library/libpthread_path = False #/g"  "$INSTALL_DIR"/calibre/PYTHON/lib/calibre/calibre/startup.py
+
+#
+cp /usr/lib/arm-linux-gnueabihf/libMagickWand-6.Q16.so.* "$INSTALL_DIR"/calibre/lib/python2.7/
 
 # we delete resources we don't use
 find "$INSTALL_DIR"/calibre/RESOURCES -type f \( -name "*.ico" -o -name "*.zip" -o -name "*.png" -o -name ".svg" -o -name "*.jpg" -o -name "*.gif" -o -name "*.css" -o -name "*.html" -o -name "*.htm" -o -name "*.js" \) -delete
