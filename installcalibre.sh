7b8df0fd0717650ef99c66d256cfc3a596cd7e8f
Emanuele Sorce <emanuele.sorce@hotmail.com>
2020-01-27 18:03:26 +0000

    Lightened calibre installation

--- installcalibre.sh
+++ installcalibre.sh
@@ -10,7 +10,6 @@ cp -r -p /usr/share/calibre/* "$INSTALL_DIR"/calibre/RESOURCES
 cp -r -p /usr/lib/python2.7/ "$INSTALL_DIR"/calibre/PYTHON/lib/
 
 # we copy only the calibre commands we use
-cp -r -p /usr/bin/calibredb "$INSTALL_DIR"/calibre/
 cp -r -p /usr/bin/calibre-customize "$INSTALL_DIR"/calibre/
 cp -r -p /usr/bin/ebook-convert "$INSTALL_DIR"/calibre/
 
@@ -21,12 +20,8 @@ cp obok_plugin.zip "$INSTALL_DIR"/calibre/
 # fix OSERR 13 error on pthread import
 sed -i "s/libpthread_path = ctypes.util.find_library/libpthread_path = False #/g" "$INSTALL_DIR"/calibre/PYTHON/lib/calibre/calibre/startup.py
 
-# fix encoding error when converting
-# ANSI_X3.4-1968
-#sed -i "s/filesystem_encoding = sys.getfilesystemencoding()/filesystem_encoding='ASCII'/g" "$INSTALL_DIR"/calibre/PYTHON/lib/calibre/calibre/constants.py
-
 # we delete resources we don't use
-# find "$INSTALL_DIR"/calibre/RESOURCES -type f \( -name "*.ico" -o -name "*.zip" -o -name "*.png" -o -name ".svg" -o -name "*.jpg" -o -name "*.gif" -o -name "*.css" -o -name "*.html" -o -name "*.htm" -o -name "*.js" \) -delete
-#find "$INSTALL_DIR"/calibre/RESOURCES -empty -type d -delete
+find "$INSTALL_DIR"/calibre/RESOURCES -type f \( -name "*.ico" -o -name "*.zip" -o -name "*.png" -o -name ".svg" -o -name "*.jpg" -o -name "*.gif" \) -delete
+find "$INSTALL_DIR"/calibre/RESOURCES -empty -type d -delete
 
 find "$INSTALL_DIR"/calibre/PYTHON -type f \( -name "*.pyc" -o -name ".pyo" \) -delete
